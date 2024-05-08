79415af8762c807c056f852337aa412b99ebf062
raianeneubern <raianeneubern@gmail.com>
2024-05-08 14:05:05 -0300

    Iniciada a camada de dados

--- Raniflix/Models/ErrorViewModel.cs
+++ Raniflix/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Raniflix.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
